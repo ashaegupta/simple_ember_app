5dab14812f83103dc09b6135ba08536468888c2d
ashaegupta <ashaegupta@gmail.com>
2013-02-07 11:39:57 -0500

    Added ember-data RESTAdapter

--- client/scripts/em.js
+++ client/scripts/em.js
@@ -1,9 +1,19 @@
 window.App = Ember.Application.create();
 
+// Use the history api so we don't need to include # in urls
 App.Router = Ember.Router.extend({
     location: 'history',
 });
 
+// Use ember-data's RESTAdapter to sync with backend
+// prefix all routes with 'api'
+App.Store = DS.Store.extend({
+    revision: 11,
+    adapter: DS.RESTAdapter.create({
+        namespace: 'api'
+    })    
+});
+
 App.Router.map(function() {
 	this.route('movies');
-})
+});
